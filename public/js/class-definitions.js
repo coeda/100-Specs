fca752cca2004c954868715b9a50c500de33e295
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-22 15:24:00 -1000

    completed gingerbread cookie

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1166,7 +1166,7 @@ var halogen = new Lightbulb(false);
 
 // Create 2 cookies of different flavors
 var chocolateChip = new Cookie('chocolate');
-var gingerbread = new Cookie();
+var gingerbread = new Cookie('gingerbread');
 
 // Create 2 different meals
 var breakfast;
