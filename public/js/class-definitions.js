9e200f6131f7599ffd0f4132ba27ac23713dc205
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-22 14:34:35 -1000

    created vehicle class

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1019,6 +1019,10 @@ function Animal(species, gender) {
  * @param {string} model The vehicle's model
  */
 
+function Vehicle(make, model){
+  this.make = make;
+  this.model = model;
+}
 
 /**
  * Step 52
