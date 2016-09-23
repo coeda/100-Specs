5688aac78f45306ee2d7010e236065b53492db96
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-22 15:15:07 -1000

    completed square

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1039,7 +1039,7 @@ function Shape(number){
   if(this.number < 3 || typeof this.number !== 'number'){
     this.sides = null;
   } else {
-    this.sides = true;
+    this.sides = number;
   }
 
 }
@@ -1140,7 +1140,7 @@ var civic = new Vehicle('Honda', 'Civic');
 var forte = new Vehicle('KIA', 'Forte');
 
 // Create 2 shapes with different numbers of sides
-var square;
+var square = new Shape(4);
 var hexagon;
 
 // Create 2 boxes
