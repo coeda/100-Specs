3241dd6de3b6544b27e8d307022df4cd1b596868
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-22 15:11:37 -1000

    created nemo

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1133,7 +1133,7 @@ function Meal(){
 
 // Create 2 different species of animals
 var george = new Animal('Monkey', 'male');
-var nemo;
+var nemo = new Animal('Fish', 'male');
 
 // Create 2 different vehicles
 var civic;
